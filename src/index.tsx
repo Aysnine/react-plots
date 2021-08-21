944ebce00790f405f285697f4249252e5ee871b0
CNine <cnine229@gmail.com>
2021-08-22 01:13:17 +0800

    refactor: extract App

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import ReactDOM from 'react-dom'
+import App from './App'
 
 import './index.css'
-
-const App = () => {
-  return <h1>Hello</h1>
-}
 
 ReactDOM.render(<App />, document.querySelector('#app'))
